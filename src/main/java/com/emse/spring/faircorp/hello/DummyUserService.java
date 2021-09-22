4a2f8423753ca123e49db656d717bac21b579a9d
Aurélien JEANNEAU <aurelienjeanneau@gmail.com>
2021-09-22 11:52:57 +0200

    UserService Commit

---
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -0,0 +1,19 @@
+package com.emse.spring.faircorp.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyUserService implements UserService {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @Override
+    public void greetAll() {
+        String [] list = {"Elodie", "Charles"};
+        for (String name: list){
+            greetingService.greet(name);
+        }
+    }
+}
