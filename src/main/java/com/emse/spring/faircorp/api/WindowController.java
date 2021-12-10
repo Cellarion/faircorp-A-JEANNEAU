35fbe92042e6f75cfbdc5489424737cd28bd178d
Cellarion <90752086+Cellarion@users.noreply.github.com>
2021-12-10 11:00:03 +0100

    revert test

--- src/main/java/com/emse/spring/faircorp/api/WindowController.java
+++ src/main/java/com/emse/spring/faircorp/api/WindowController.java
@@ -25,7 +25,7 @@ public class WindowController {
         this.roomDao = roomDao;
     }
 
-    @GetMapping(windows)
+    @GetMapping
     public List<WindowDto> findAll() {
         return windowDao.findAll().stream().map(WindowDto::new).collect(Collectors.toList());
     }
